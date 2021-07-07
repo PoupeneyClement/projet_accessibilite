cc2a0366ff573608b77d4fdc7be01318cb7fcaed
etiennezastko <80699659+etiennezastko@users.noreply.github.com>
2021-07-07 11:02:56 +0200

    Delete gwendoline-wordpress/wp-content/themes/theme enfant directory

--- gwendoline-wordpress/wp-content/themes/theme enfant/404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-
-<p>Cette page est 404.php (donc toute les urls qui n'existent pas)</p>
-
-
-
-<?php get_footer(); ?>
--- gwendoline-wordpress/wp-content/themes/theme enfant/front-page.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-
-<p>Cette page est front-page.php (donc la vrai page d'accueil)</p>
-
-
-
-<?php get_footer(); ?>
